2e0fb6bf780a00338e0ff4b7c062f3b2febf9ab3
John Barnette <jbarnette@gmail.com>
2009-06-23 12:07:19 -0700

    Make the git:tag task respect a TAG env var.

--- lib/hoe/git.rb
+++ lib/hoe/git.rb
@@ -1,14 +1,16 @@
 # Tasks provided:
 #
-# * <tt>git:tag</tt> -- Tag the current version and push the tag to all
-#   the <tt>git_remotes</tt>. This task is added as a dependency of
-#   the built-in <tt>release</tt> task.
+# * <tt>git:tag</tt> -- Tag the current version and push the tag to
+#   all the <tt>git_remotes</tt>. This task is added as a dependency
+#   of the built-in <tt>release</tt> task. The tag will be
+#   <tt>"v#{version}"</tt> unless <tt>TAG</tt> or <tt>VERSION</tt> are
+#   set in the environment.
 
 module Hoe::Git
   VERSION = "1.0.0"
 
   ##
-  # Optional: Which remotes do you want to push tags, etc to?
+  # Optional: Which remotes do you want to push tags, etc. to?
   # [default: %w(origin)]
 
   attr_accessor :git_remotes
@@ -18,13 +20,12 @@ module Hoe::Git
   end
 
   def define_git_tasks
-    desc "Create and push a tag (default #{version})."
+    desc "Create and push a TAG (default v#{version})."
     task "git:tag" do
-      t = "v#{version}"
-      v = ENV["VERSION"] || version
+      tag = ENV["TAG"] || "v#{ENV["VERSION"] || version}"
 
-      sh "git tag -f #{t}"
-      git_remotes.each { |r| sh "git push -f #{r} tag #{t}" }
+      sh "git tag -f #{tag}"
+      git_remotes.each { |remote| sh "git push -f #{remote} tag #{tag}" }
     end
 
     task :release => "git:tag"
