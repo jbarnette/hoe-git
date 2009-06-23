38ea4c7550487a76227c8dfbdcad7285d058aa9c
John Barnette <jbarnette@gmail.com>
2009-06-23 01:20:53 -0700

    Minor cleanups.

--- lib/hoe/git.rb
+++ lib/hoe/git.rb
@@ -1,6 +1,6 @@
 # Tasks provided:
 #
-# * <tt>git:tag</t> -- Tag the current version and push the tag to all
+# * <tt>git:tag</tt> -- Tag the current version and push the tag to all
 #   the <tt>git_remotes</tt>. This task is added as a dependency of
 #   the built-in <tt>release</tt> task.
 
@@ -18,13 +18,13 @@ module Hoe::Git
   end
 
   def define_git_tasks
-    desc "Create and push a v#{version} tag."
+    desc "Create and push a tag (default #{version})."
     task "git:tag" do
       t = "v#{version}"
       v = ENV["VERSION"] || version
 
-      warn "git tag -f #{t}"
-      git_remotes.each { |r| warn "git push -f #{r} tag #{t}" }
+      sh "git tag -f #{t}"
+      git_remotes.each { |r| sh "git push -f #{r} tag #{t}" }
     end
 
     task :release => "git:tag"
