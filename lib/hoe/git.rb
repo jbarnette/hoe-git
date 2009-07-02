8f53162829347ad6c9c1505db0f68ca2de379364
John Barnette <jbarnette@gmail.com>
2009-07-01 18:54:57 -0700

    Prepping for 1.2.0.

--- lib/hoe/git.rb
+++ lib/hoe/git.rb
@@ -13,7 +13,7 @@ class Hoe #:nodoc:
   module Git
 
     # Duh.
-    VERSION = "1.1.3"
+    VERSION = "1.2.0"
 
     # What do you want at the front of your release tags?
     # [default: <tt>"v"</tt>]
