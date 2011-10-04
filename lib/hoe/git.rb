71e428403af2fe9bec4414bbde36a8b3a6f9bac3
Ryan Davis <ryand-ruby@zenspider.com>
2011-10-04 13:55:58 -0700

    prepped for release

--- lib/hoe/git.rb
+++ lib/hoe/git.rb
@@ -20,7 +20,7 @@ class Hoe #:nodoc:
   module Git
 
     # Duh.
-    VERSION = "1.4.0"
+    VERSION = "1.4.1"
 
     # What do you want at the front of your release tags?
     # [default: <tt>"v"</tt>]
